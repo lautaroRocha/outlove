3d5079506cade9da2f514511982a3b402521e1c9
Lautaro Rocha <lautarovrocha@gmail.com>
2022-05-10 17:29:11 -0300

    primer ejercicio javascript

---
+++ primer.js
@@ -0,0 +1,24 @@
+nivelAfecto = prompt("en una escala del uno al cinco, cuán apegado se siente a su Alaskita?")
+
+if (nivelAfecto >= 0 && nivelAfecto < 1) {
+    alert("llamaron del Infierno, ya vienen por usted.")
+}
+else if (nivelAfecto >= 1 && nivelAfecto < 2) {
+    alert("lamentamos mucho el accidente traumático que seguramente usted sufrió y perdió así la capacidad de goce.")
+}
+else if (nivelAfecto >= 2 && nivelAfecto < 3) {
+    alert("haga un esfuerzo y permítase ser más feliz.")
+}
+else if (nivelAfecto >= 3 && nivelAfecto < 4) {
+    alert("no está mal, pero puede mejorar.")
+}
+else if (nivelAfecto >= 4 && nivelAfecto< 5) {
+    alert("es tan brillante... es esto lo que llaman amor?!?!?!?")
+}
+else if (nivelAfecto ==5)  {
+    alert ("íbamos a felicitarlo, pero usted ya conoce la dimensión de su dicha. dios lo bediga.")
+}
+
+else if (nivelAfecto >=5)  {
+    alert ("le dijimos del uno al cinco. en breve lo estaremos recogiendo. no se resista.")
+}
