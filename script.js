6a7166cc618a4fa2308407e8912139917c28d040
Lautaro Rocha <lautarovrocha@gmail.com>
2022-06-05 18:56:00 -0300

    añado nuevo elemento h1 al carrusel (htmlv css js)

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 let imagenCarrusel = document.querySelector("#imagen-carrusel")
 const imagenesCarru = ["test-uno.png", "test-dos.png", "test-tres.png"];
 let botonCarrusel = document.querySelector("#boton-carrusel");
-botonCarrusel.addEventListener('click', pasarFoto);
 let posicion = 0
+let textoCarru = document.querySelector("#copy-carru")
+const copyCarru = ["Una aventura te espera", "Todo lo mejor para tus viajes", "A donde la tierra te lleve"]
+
+botonCarrusel.addEventListener('click', pasarFoto);
+
 function pasarFoto(){
     if (posicion >= imagenesCarru.length-1){
         posicion = 0;
@@ -11,24 +15,26 @@ function pasarFoto(){
         posicion ++
     }
     cambiarFoto();
+    cambiarCopy();
     }
 function cambiarFoto(){
     imagenCarrusel.setAttribute('src' , `images/${imagenesCarru[posicion]}` )
 }
-
-
+function cambiarCopy() {
+    textoCarru.textContent = copyCarru[posicion];
+}
 //DISPLAY DE PRODUCTOS Y SELECCIÓN
-const productos = ["remera1.jpg", "remera2.jpg", "remera3.jpg", "remera4.jpg", "remera5.jpg", "remera6.jpg" ];
+const productos = ["remera1", "remera2", "remera3", "remera4", "remera5", "remera6" ];
 
 const productoSeleccionado = document.querySelector('#producto-seleccion');
 const fotosProductos = document.querySelector('.tienda-fotos');
     
 for (let i=0; i<productos.length; i++) {
     let nuevaFoto = document.createElement('img');
-    nuevaFoto.setAttribute('src', `images/${productos[i]}`);
+    nuevaFoto.setAttribute('src', `images/${productos[i]}.png`);
     fotosProductos.appendChild(nuevaFoto);
     nuevaFoto.addEventListener('click', () => 
-    productoSeleccionado.setAttribute('src' , `images/${productos[i]}`))
+    productoSeleccionado.setAttribute('src' , `images/${productos[i]}.png`))
 };
 
 
