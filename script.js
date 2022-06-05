0868f80239605bac1e4c3122ebfce997d136d266
Lautaro Rocha <lautarovrocha@gmail.com>
2022-06-05 00:30:53 -0300

    carrusel del inicio (imgs editadas y js)

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-//DISPLAY DE PRODUCTOS Y SELECCIÓN
-const productos = ["images/remera1.jpg", "images/remera2.jpg", "images/remera3.jpg", "images/remera4.jpg", "images/remera5.jpg", "images/remera6.jpg" ];
+//CARRUSEL DE BANNER 
+let imagenCarrusel = document.querySelector("#imagen-carrusel")
+const imagenesCarru = ["test-uno.png", "test-dos.png", "test-tres.png"];
+let botonCarrusel = document.querySelector("#boton-carrusel");
+botonCarrusel.addEventListener('click', pasarFoto);
+let posicion = 0
+function pasarFoto(){
+    if (posicion >= imagenesCarru.length-1){
+        posicion = 0;
+    }else {
+        posicion ++
+    }
+    cambiarFoto();
+    }
+function cambiarFoto(){
+    imagenCarrusel.setAttribute('src' , `images/${imagenesCarru[posicion]}` )
+}
 
-const productoSeleccionado = document.getElementById('producto-seleccion');
+
+//DISPLAY DE PRODUCTOS Y SELECCIÓN
+const productos = ["remera1.jpg", "remera2.jpg", "remera3.jpg", "remera4.jpg", "remera5.jpg", "remera6.jpg" ];
+
+const productoSeleccionado = document.querySelector('#producto-seleccion');
 const fotosProductos = document.querySelector('.tienda-fotos');
     
 for (let i=0; i<productos.length; i++) {
     let nuevaFoto = document.createElement('img');
-    nuevaFoto.setAttribute('src', productos[i]);
+    nuevaFoto.setAttribute('src', `images/${productos[i]}`);
     fotosProductos.appendChild(nuevaFoto);
     nuevaFoto.addEventListener('click', () => 
-    productoSeleccionado.setAttribute('src' , productos[i]))
+    productoSeleccionado.setAttribute('src' , `images/${productos[i]}`))
 };
 
 
-//VARIACION EN EL TITULO INTERACTUANDO CON LAS FOTOS
 
-const titulo = document.getElementById('titu');
-const fotosInicio = document.querySelector('.inicio-fotos');
-
-fotosInicio.addEventListener('click', () =>{
-    if (titulo.textContent ==='Alaska, el gatito que todos aman amar.') {
-        return titulo.textContent = "¡Atrevéte! Alaska aguarda ansiosamente amarte"
-    }else if(titulo.textContent === "¡Atrevéte! Alaska aguarda ansiosamente amarte"){
-        return titulo.textContent = "COMPRA COMPRA COMPRA COMPRA" 
-    } else {
-        return titulo.textContent = 'Alaska, el gatito que todos aman amar.';
-    }
-})
 
 
 
