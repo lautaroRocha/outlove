bf3defbabe5c7b314200f724e01a9e865036404d
Lautaro Rocha <lautarovrocha@gmail.com>
2022-06-17 12:19:11 -0300

    funcion y variables para nombre de producto seleccionado

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-//PRIMER PRENTREGA 
-function saludarUsuario() {
-    let nombre = prompt(`¡Bienvenido a Outlove! Nos gustaría conocerte un poco más, cómo te llamás?`);
-    if(nombre == null || nombre == ""){
-        nombre = prompt(`No queremos molestarte, nos encantaría saber tu nombre.`)
-    }
-        let pregunta = confirm(`Hola ${nombre}, ¿te gustaría recibir las últimas ofertas en tu correo?`)
-   
-        if(pregunta){
-            alert(`¡Qué bueno! ¡Estaremos en contacto!`)
-        } else {
-            alert(`¡Está bien, disfruta del sitio!`)
-        }
-    } 
-    ;
-
-
-
-
-
 
 //CARRUSEL DE BANNER 
-{let imagenCarrusel = document.querySelector("#imagen-carrusel")
+let imagenCarrusel = document.querySelector("#imagen-carrusel")
 const imagenesCarru = ["test-uno.png", "test-dos.png", "test-tres.png"];
 let botonCarrusel = document.querySelector("#boton-carrusel");
 let posicion = 0
@@ -43,20 +23,91 @@ function cambiarFoto(){
 }
 function cambiarCopy() {
     textoCarru.textContent = copyCarru[posicion];
-}}
+}
 //DISPLAY DE PRODUCTOS Y SELECCIÓN
-{const productos = ["remera1", "remera2", "remera3", "remera4", "remera5", "remera6" ];
+
+const nombreProducto = document.querySelector('#producto-seleccion-nombre');
+
+const productos = ["remera1", "remera2", "remera3", "remera4", "remera5", "remera6" ];
+
 
 const productoSeleccionado = document.querySelector('#producto-seleccion');
 const fotosProductos = document.querySelector('.tienda-fotos');
+
+
+function actualizarNombre() {
+    switch (productoSeleccionado.getAttribute('src')){
+        case "images/remera1.png":
+        nombreProducto.textContent = "remera negra";
+        break;
+        case "images/remera2.png":
+        nombreProducto.textContent = "remera blanca";
+        break;
+        case "images/remera3.png":
+        nombreProducto.textContent = "remera caminante";
+        break;
+        case "images/remera4.png":
+        nombreProducto.textContent = "remera emblema";
+        break;
+        case "images/remera5.png":
+        nombreProducto.textContent = "remera caminante blanca";
+        break;
+        case "images/remera6.png":
+        nombreProducto.textContent = "remera montaña negra blanca";
+        break;
+        default : 
+        nombreProducto.textContent = "producto";
+    }
+    }
     
+
 for (let i=0; i<productos.length; i++) {
     let nuevaFoto = document.createElement('img');
     nuevaFoto.setAttribute('src', `images/${productos[i]}.png`);
     fotosProductos.appendChild(nuevaFoto);
     nuevaFoto.addEventListener('click', () => 
     productoSeleccionado.setAttribute('src' , `images/${productos[i]}.png`))
+    nuevaFoto.addEventListener('click', actualizarNombre)
 };
 
+
+
+
+
+
+
+
+
+//PASA EL PRODUCTO SELECCIONADO
+//AL CARRITO
+
+class Pedido {
+    constructor(cliente, direccion, producto, cantidad, talle, observaciones){
+        this.cliente = cliente,
+        this.direccion = direccion,
+        this.producto = producto,
+        this.cantidad = cantidad,
+        this.talle = talle,
+        this.observaciones = observaciones
+    }
 }
+
+let carrito = {};
+
+class Producto {
+    constructor(modelo, talle, cantidad, precio, observaciones, cliente, direccion){
+        this.modelo = modelo,
+        this.talle = talle,
+        this.cantidad = cantidad, 
+        this.precio = precio,
+        this.observaciones = observaciones,
+        this.cliente = cliente,
+        this.direccion = direccion
+    }
+    añadirAlCarro() {
+        carrito = new Pedido (this.cliente, this.direccion, this.modelo, this.cantidad, this.talle, this.observaciones)
+    }
+}
+
+const añadirCarrito = document.querySelector("#añadir-carrito")
 
