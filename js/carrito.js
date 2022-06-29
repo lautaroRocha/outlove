6a93cb50d80cae54b4d95adb36a225d102d0ed65
Lautaro Rocha <lautarovrocha@gmail.com>
2022-06-29 15:54:32 -0300

    eliminar ultimo objeto del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -26,15 +26,17 @@ const productoSeleccionado = document.querySelector('#producto-seleccion');
 const fotosProductos = document.querySelector('.tienda-fotos');
 
 //Productos disponibles
-let remeraNegraMontaña = new Producto("remera negra montaña",  1700, "remera1");
-let remeraBlancaMontaña = new Producto("remera blanca montaña", 1700, "remera2");
-let remeraCaminante = new Producto("remera caminante", 1700, "remera3");
-let remeraEmblema = new Producto("remera emblema", 1700, "remera4");
-let remeraCaminanteBlanca = new Producto("remera caminante blanca", 1700, "remera5");
-let remeraNegraMontañaBlanca = new Producto("remera negra montaña blanca", 1700, "remera6");
-const productos = [remeraNegraMontaña,remeraBlancaMontaña,remeraCaminante,remeraEmblema,remeraCaminanteBlanca, remeraNegraMontañaBlanca];
+const productos = [];
+
+productos.push(remeraNegraMontaña = new Producto("remera negra montaña",  1700, "remera1"));
+productos.push(remeraBlancaMontaña = new Producto("remera blanca montaña", 1700, "remera2"));
+productos.push(remeraCaminante = new Producto("remera caminante", 1700, "remera3"));
+productos.push(remeraEmblema = new Producto("remera emblema", 1700, "remera4"));
+productos.push(remeraCaminanteBlanca = new Producto("remera caminante blanca", 1700, "remera5"));
+productos.push(remeraNegraMontañaBlanca = new Producto("remera negra montaña blanca", 1700, "remera6"));
 
 //carrito
+let datosClientes = document.querySelector("#forma-cliente")
 const botonAnadir = document.querySelector("#anadir-carrito");
 let carrito = [];
 
@@ -43,9 +45,9 @@ let carrito = [];
 let botonModal = document.querySelector('#boton-modal')
 let carritoModal = document.querySelector('#carrito-modal')
 let botonCerrarModal = document.querySelector("#cerrar-modal")
-
-
 let cantidadCompra = document.querySelector("#cantidad-comprada");
+let divFotos = document.querySelector("#carrito-flex");
+let btnEliminar = document.querySelector('#eli-prod');
 
 //El modelo del producto seleccionado
 function actualizarNombre() {
@@ -90,17 +92,15 @@ for (let i=0; i<productos.length; i++) {
 function sumarCantidad() {
     cantidadCompra.textContent = carrito.reduce( (ac, pedido) => ac + parseInt(pedido.cantidad), 0);
 }
+
 //envía img seleccionada al
 //carrito
 function llenarCarrito(){
-    let divFotos = document.querySelector("#carrito-flex")
-    
     let imagenCompra = document.createElement('img')
-
     imagenCompra.setAttribute('src', productoSeleccionado.getAttribute('src'));
-   
     divFotos.appendChild(imagenCompra); 
 }
+
 //envia los datos del form
 //al carrit
 function enviarAlCarrito() {
@@ -123,14 +123,18 @@ function enviarAlCarrito() {
 
 }
 
+///enviar datos
+datosClientes.addEventListener('submit', enviarAlCarrito);
 
-let btnAnadir = document.querySelector('#anadir-carrito');
-
-btnAnadir.addEventListener('click', enviarAlCarrito);
-
+//botones del modal
 botonModal.onclick = function() {
     carritoModal.style.display = "block";
   }
 botonCerrarModal.onclick = function() {
     carritoModal.style.display = "none";
+}
+btnEliminar.onclick = function (){
+    carrito.pop()
+    divFotos.removeChild(divFotos.lastChild)
+    sumarCantidad()
 }
