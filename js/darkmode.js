39c748c6e3ca031250ee37b605c7fd4f40a0df22
Lautaro Rocha <lautarovrocha@gmail.com>
2022-06-28 22:16:15 -0300

    incluyo las hojas del copy al darkmode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,13 +1,15 @@
 let botonMode = document.querySelector("#btn-mode")
-
+let hojasCopy = document.querySelectorAll("#hoja")
 let bodyTag = document.querySelector("body");
 
 function cambiarModo() {
     let modo = bodyTag.getAttribute('class');
     if (modo == "light"){
         bodyTag.setAttribute('class', 'dark')
+        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-osc'))
     }else {
-        bodyTag.setAttribute('class', 'light')
+        bodyTag.setAttribute('class', 'light');
+        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-claro'));
     }
 }
 
