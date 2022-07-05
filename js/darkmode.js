73c2ae4a5f34143b994c06eae320b2167401e85e
Lautaro Rocha <lautarovrocha@gmail.com>
2022-07-05 20:47:09 -0300

    guarda preferencia en la paleta

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,15 +1,28 @@
+let preferenciaModo = localStorage.getItem('mode');
 let botonMode = document.querySelector("#btn-mode")
 let hojasCopy = document.querySelectorAll("#hoja")
 let bodyTag = document.querySelector("body");
 
-botonMode.onclick = () => {
-    modo = bodyTag.getAttribute('class');
-    if (modo == "light"){
+    if (preferenciaModo === "dark"){
         bodyTag.setAttribute('class', 'dark')
         hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-osc'))
     }else {
+        bodyTag.setAttribute('class', 'light')
+        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-claro'))
+    }
+
+ botonMode.onclick = () => {
+    modo = bodyTag.getAttribute('class');
+    if (modo == "light" || modo == " "){
+        localStorage.setItem('mode', "dark")
+        bodyTag.setAttribute('class', 'dark')
+        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-osc'))
+    }else if (modo == "dark"){
+        localStorage.setItem('mode', "light")
         bodyTag.setAttribute('class', 'light');
-        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-claro'));
+        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-claro'))
     }
 }
+
+
 
