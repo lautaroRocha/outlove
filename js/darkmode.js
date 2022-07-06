d281dadff9a0b7b38db29c76cbdeb1da04a9ae94
Lautaro Rocha <lautarovrocha@gmail.com>
2022-07-06 17:47:12 -0300

    mejora sintáctica

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,16 +1,20 @@
 let preferenciaModo = localStorage.getItem('mode');
+
 let botonMode = document.querySelector("#btn-mode")
 let hojasCopy = document.querySelectorAll("#hoja")
 let bodyTag = document.querySelector("body");
 
-    if (preferenciaModo === "dark"){
+//recuerda preferencia de modo
+window.onload = () => {
+    if (preferenciaModo == "dark"){
         bodyTag.setAttribute('class', 'dark')
         hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-osc'))
-    }else {
+}else{
         bodyTag.setAttribute('class', 'light')
         hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-claro'))
-    }
-
+}
+}
+//tooglea entre modos
  botonMode.onclick = () => {
     modo = bodyTag.getAttribute('class');
     if (modo == "light"){
