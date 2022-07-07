7c7d5de9f8f84de4588041635b3dcc5cefec27e6
Lautaro Rocha <lautarovrocha@gmail.com>
2022-07-07 17:13:05 -0300

    modulación de carrusel.js

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,17 +1,12 @@
-//CARRUSEL DE BANNER 
+import {imagenCarrusel, IMAGENES_CARRU, botonCarrusel, textoCarru, copyCarru} from "./modules/var_carrusel.js";
 
-let imagenCarrusel = document.querySelector("#imagen-carrusel")
-const IMAGENES_CARRU = ["test-uno.png", "test-dos.png", "test-tres.png"];
-let botonCarrusel = document.querySelector("#boton-carrusel");
-let posicion = 0
-let textoCarru = document.querySelector("#copy-carru")
-const copyCarru = ["Una aventura te espera", "Todo lo mejor para tus viajes", "A donde la tierra te lleve"]
-    
+let posicion = 0;
+
 botonCarrusel.onclick= () => {
         if (posicion >= IMAGENES_CARRU.length-1){
             posicion = 0;
         }else {
-            posicion ++
+            posicion ++;
         }
         cambiarFoto();
         cambiarCopy();
