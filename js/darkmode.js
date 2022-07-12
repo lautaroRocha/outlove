df8520447cc70dba2b251f99e56cb7ef1874c3b3
Lautaro Rocha <lautarovrocha@gmail.com>
2022-07-12 20:38:43 -0300

    operador ternario

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,28 +1,18 @@
-import { preferenciaModo, botonMode, hojasCopy, bodyTag } from "./modules/var_darkmode.js";
+import { preferenciaModo, botonMode, bodyTag, setDark, setLight, persistirDark, persistirLight  } from "./modules/var_darkmode.js";
 
 //revisa si hay un modo preferido guardado
+
+
 window.onload = () => {
-    if (preferenciaModo === "dark"){
-        bodyTag.setAttribute('class', 'dark')
-        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-osc'))
-    }else {
-        bodyTag.setAttribute('class', 'light')
-        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-claro'))
-    }
+    preferenciaModo === "dark" ? setDark() : setLight();
 }
+
+
 //cambia entre modos y setea la preferencia
  botonMode.onclick = () => {
     let modo = bodyTag.getAttribute('class');
-    if (modo == "light"){
-        localStorage.setItem('mode', "dark")
-        bodyTag.setAttribute('class', 'dark')
-        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-osc'))
-    }else if (modo == "dark"){
-        localStorage.setItem('mode', "light")
-        bodyTag.setAttribute('class', 'light');
-        hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-claro'))
-    }
-}
+    modo === "light" ? persistirDark() : persistirLight();
+ }
 
 
 
--- js/modules/var_darkmode.js
+++ js/modules/var_darkmode.js
@@ -2,3 +2,24 @@ export let preferenciaModo = localStorage.getItem('mode');
 export let botonMode = document.querySelector("#btn-mode")
 export let hojasCopy = document.querySelectorAll("#hoja")
 export let bodyTag = document.querySelector("body");
+
+export function setDark(){
+    bodyTag.setAttribute('class', 'dark')
+    hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-osc'))
+}
+
+export function setLight(){
+    bodyTag.setAttribute('class', 'light')
+    hojasCopy.forEach( hoja => hoja.setAttribute('class', 'fondo-hojas-claro'))
+}
+
+export function persistirDark(){
+    setDark()
+    localStorage.setItem('mode', "dark");
+}
+
+export function persistirLight(){
+    setLight()
+    localStorage.setItem('mode', "light")
+}
+
