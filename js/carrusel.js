a1abdf2289e00a331e3380e74c8211fded41367b
Lautaro Rocha <lautarovrocha@gmail.com>
2022-07-19 21:23:06 -0300

    agrego fadeIn al carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -2,20 +2,41 @@ import {imagenCarrusel, IMAGENES_CARRU, botonCarrusel, textoCarru, copyCarru} fr
 
 let posicion = 0;
 
+// function fadeIn(ele){
+//     ele.style.opacity = 0;
+//     function tick() {
+//       ele.style.opacity = +ele.style.opacity + 0.01;
+//       if (ele.style.opacity < 1) {
+//         (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 1000)
+//       }
+//     };
+//     tick();
+// }
+
 botonCarrusel.onclick= () => {
-        if (posicion >= IMAGENES_CARRU.length-1){
-            posicion = 0;
-        }else {
-            posicion ++;
-        }
+        posicion >= IMAGENES_CARRU.length-1 ?posicion = 0 : posicion ++;
         cambiarFoto();
         cambiarCopy();
         }
 
 function cambiarFoto(){
-            imagenCarrusel.setAttribute('src' , `images/${IMAGENES_CARRU[posicion]}` )
+        fadeIn(imagenCarrusel)
+        imagenCarrusel.setAttribute('src' , `images/${IMAGENES_CARRU[posicion]}` )
         }
 function cambiarCopy() {
+            fadeIn(textoCarru)
             textoCarru.textContent = copyCarru[posicion];
+
         }
-    
+function fadeIn(ele){
+    ele.style.opacity = 0;
+    function tick() {
+        ele.style.opacity = +ele.style.opacity + 0.01;
+    if (ele.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+        };
+    }
+    tick();
+}
+
+
